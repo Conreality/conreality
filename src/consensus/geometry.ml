6a96c3f6e8dec20ad29a32afa559d5f3d0ce03df
Arto Bendiken <arto@bendiken.net>
2015-11-02 05:19:34 +0100

    Fleshed out the Vector interface.

--- src/consensus/geometry.ml
+++ src/consensus/geometry.ml
@@ -1,15 +1,34 @@
 (* This is free and unencumbered software released into the public domain. *)
 
+type vector = { x: float; y: float; z: float }
+type point  = vector
+
 module Vector = struct
-  type t = { x: float; y: float; z: float }
+  type t = vector
 
   let create x y z = { x; y; z }
 
-  let invert self = create (-. self.x) (-. self.y) (-. self.z)
+  let x v = v.x
+  let y v = v.y
+  let z v = v.z
+
+  let zero () =
+    create 0. 0. 0.
+
+  let ( + ) a b =
+    create (a.x +. b.x) (a.y +. b.y) (a.z +. b.z)
+
+  let ( - ) a b =
+    create (a.x -. b.x) (a.y -. b.y) (a.z -. b.z)
+
+  let invert v =
+    create (-. v.x) (-. v.y) (-. v.z)
+
+  let magnitude v =
+    sqrt (v.x *. v.x) +. (v.y *. v.y) +. (v.z *. v.z)
+
+  let normalize v =
+    v (* TODO *)
 end
 
 module Point = Vector
-
-type point = Point.t
-
-type vector = Vector.t
--- src/consensus/geometry.mli
+++ src/consensus/geometry.mli
@@ -1,4 +1,22 @@
 (* This is free and unencumbered software released into the public domain. *)
 
-type point      (** 3D point. *)
 type vector     (** 3D vector. *)
+type point      (** 3D point. *)
+
+module Vector : sig
+  type t = vector
+  val create : float -> float -> float -> t
+  val x : vector -> float
+  val y : vector -> float
+  val z : vector -> float
+  val zero : unit -> t
+  val ( + ) : t -> t -> t
+  val ( - ) : t -> t -> t
+  val invert : vector -> vector
+  val magnitude : vector -> float
+  val normalize : vector -> vector
+end
+
+module Point : sig
+  type t = point
+end
