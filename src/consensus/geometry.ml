4652a1933e0a7a1f75bae13fbb2adcfe8895e562
Arto Bendiken <arto@bendiken.net>
2015-11-02 04:12:53 +0100

    Implemented Vector.create and Vector.invert.

--- src/consensus/geometry.ml
+++ src/consensus/geometry.ml
@@ -1,7 +1,15 @@
 (* This is free and unencumbered software released into the public domain. *)
 
-module Point = struct
+module Vector = struct
   type t = { x: float; y: float; z: float }
+
+  let create x y z = { x; y; z }
+
+  let invert self = create (-. self.x) (-. self.y) (-. self.z)
 end
 
+module Point = Vector
+
 type point = Point.t
+
+type vector = Vector.t
--- src/consensus/geometry.mli
+++ src/consensus/geometry.mli
@@ -1,3 +1,4 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 type point      (** 3D point. *)
+type vector     (** 3D vector. *)
