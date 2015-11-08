8737c427fbd97e9b433d807a73eb75d55bcc256a
Arto Bendiken <arto@bendiken.net>
2015-11-08 20:42:31 +0100

    Added some initial tests for the Geometry module.

--- test/check_geometry.ml
+++ test/check_geometry.ml
@@ -1,3 +1,17 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 open Consensus.Geometry
+
+let create () =
+  Alcotest.(check int) "same int" 0 (int_of_float (Vector.x (Vector.zero ())))
+
+let equals () =
+  Alcotest.(check (bool)) "same bool" true ((Vector.zero ()) = (Vector.zero ()))
+
+let () =
+  Alcotest.run "My first test" [
+    "test_set", [
+      "Creation", `Quick, create;
+      "Equality", `Quick, equals;
+    ];
+  ]
