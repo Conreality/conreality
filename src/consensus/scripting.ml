fdc349ff8c31b33a13de54b06d412254cf71dbb1
Arto Bendiken <arto@bendiken.net>
2015-11-10 11:17:02 +0100

    Implemented Context.create.

--- src/consensus/scripting.ml
+++ src/consensus/scripting.ml
@@ -1,1 +1,13 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+open Lua_api
+
+module Context = struct
+  type t = Lua_api_lib.state
+
+  let create () =
+    LuaL.newstate ()
+
+  let load_file context filepath =
+    ()
+end
--- src/consensus/scripting.mli
+++ src/consensus/scripting.mli
@@ -1,1 +1,7 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+module Context : sig
+  type t
+  val create : unit -> t
+  val load_file : t -> string -> unit
+end
