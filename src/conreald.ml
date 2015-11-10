5960acd3e7a9ba5c99895f675bc0c67ec915437b
Arto Bendiken <arto@bendiken.net>
2015-11-10 07:46:31 +0100

    Defined the `conreald` daemon service binary.

---
+++ src/conreald.ml
@@ -0,0 +1,66 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Cmdliner
+open Consensus
+
+(* Configuration *)
+
+let version = "0.0.0"
+
+let man_sections = [
+  `S "DESCRIPTION";
+  `P "Runs the Conreality daemon.";
+  `S "BUGS";
+  `P "Check open bug reports at <https://github.com/conreality/consensus/issues>.";
+  `S "SEE ALSO";
+  `P "$(b,concfg)(8), $(b,conctl)(8)";
+]
+
+(* Option types *)
+
+type verbosity = Normal | Quiet | Verbose
+
+type common_options = { debug: bool; verbosity: verbosity }
+
+let str = Printf.sprintf
+
+let verbosity_str = function
+  | Normal -> "normal" | Quiet -> "quiet" | Verbose -> "verbose"
+
+(* Command implementations *)
+
+let conreald options mission =
+  Printf.printf "Hello, world!\n" (* TODO *)
+
+(* Options common to all commands *)
+
+let common_options debug verbosity = { debug; verbosity }
+
+let common_options_term =
+  let debug =
+    let doc = "Enable debugging output." in
+    Arg.(value & flag & info ["debug"] ~doc)
+  in
+  let verbosity =
+    let doc = "Suppress informational output." in
+    let quiet = Quiet, Arg.info ["q"; "quiet"] ~doc in
+    let doc = "Give verbose output." in
+    let verbose = Verbose, Arg.info ["v"; "verbose"] ~doc in
+    Arg.(last & vflag_all [Normal] [quiet; verbose])
+  in
+  Term.(const common_options $ debug $ verbosity)
+
+(* Command definitions *)
+
+let command =
+  let mission =
+    let doc = "A file path to a mission scenario script." in
+    Arg.(value & pos 0 string "" & info [] ~docv:"MISSION" ~doc)
+  in
+  let doc = "Conreality daemon." in
+  let man = man_sections in
+  Term.(const conreald $ common_options_term $ mission),
+  Term.info "conreald" ~version ~doc ~man
+
+let () =
+  match Term.eval command with `Error _ -> exit 1 | _  -> exit 0
