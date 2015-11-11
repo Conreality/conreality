618df8998bad5037e16d6a4508c1dc93a4b17016
Mike Gogulski <mike@gogulski.com>
2015-11-12 00:55:01 +0100

    more vector tests, now with errors!

--- test/check_geometry.ml
+++ test/check_geometry.ml
@@ -22,11 +22,11 @@ let tvec3_2 = V3.create e pi phi
 let tvec3_0 = V3.zero
 
 let v3_create () = Alcotest.(check (list float)) "float list" [e; pi; phi] ([(V3.x tvec3_2); (V3.y tvec3_2); (V3.z tvec3_2)])
-let v3_x () = Alcotest.(check int) "same int" 3 (int_of_float (V3.x tvec3_1))
-let v3_y () = Alcotest.(check int) "same int" 1 (int_of_float (V3.y tvec3_1))
-let v3_z () = Alcotest.(check int) "same int" 2 (int_of_float (V3.z tvec3_1))
+let v3_x () = Alcotest.(check float) "same float" 3. (V3.x tvec3_1)
+let v3_y () = Alcotest.(check float) "same float" 3.14159 (V3.y tvec3_2)
+let v3_z () = Alcotest.(check float) "same float" 2. (V3.z tvec3_1)
 let v3_el () = Alcotest.(check int) "same int" 3 (int_of_float (V3.x tvec3_1))
-let v3_zero () = Alcotest.(check (list float)) "float list" [0.; 0.; 0.;] ([V3.x tvec3_0; V3.y tvec3_0; V3.z tvec3_0])
+let v3_zero () = Alcotest.(check (list float)) "float list" [0.; 0.; 0.] ([V3.x tvec3_0; V3.y tvec3_0; V3.z tvec3_0])
 let v3_unitx () = Alcotest.(check bool) "same bool" true (V3.eq V3.unitx (V3.create 1. 0. 0.))
 let v3_unity () = Alcotest.(check bool) "same bool" true (V3.eq V3.unity (V3.create 0. 1. 0.))
 let v3_unitz () = Alcotest.(check bool) "same bool" true (V3.eq V3.unitz (V3.create 0. 0. 1.))
@@ -36,22 +36,29 @@ let v3_add () = let v = V3.add tvec3_1 tvec3_2 in Alcotest.(check (list float)) 
 (* TODO: Find out how to test infix operators *)
 (*let v3_op_add () = let v = tvec3_1 + tvec3_2 in Alcotest.(check (list float)) "float list" [5.71828; 4.14159; 3.61803] ([(V3.x v); (V3.y v); (V3.z v)])*)
 (* TODO: Find out why this is failing in the float module above(?) *)
-(*let v3_sub () = let v = V3.sub tvec3_1 tvec3_2 in Alcotest.(check (list float)) "float list" [0.28172; -2.14159; 0.38197] ([(V3.x v); (V3.y v); (V3.z v)])*)
+let v3_sub () = let v = V3.sub tvec3_1 tvec3_2 in Alcotest.(check (list float)) "float list" [0.28172; -2.14159; 0.38197] ([(V3.x v); (V3.y v); (V3.z v)])
+let v3_sub2 () = let v = V3.sub tvec3_1 tvec3_1 in Alcotest.(check (list float)) "float list" [0.; 0.; 0.] ([V3.x v; V3.y v; V3.z v])
 let v3_op_sub () = todo ()
 let v3_eq () = Alcotest.(check bool) "same bool" true (V3.eq V3.zero V3.zero)
 let v3_op_eq () = Alcotest.(check bool) "same bool" true ((V3.zero) = (V3.zero))
 let v3_float_equals () = Alcotest.(check float) "same float" e (V3.x tvec3_2)
-let v3_smul () = let v = V3.smul tvec3_1 2. in Alcotest.(check (list float)) "float list" [6.; 2.; 4.;] ([(V3.x v); (V3.y v); (V3.z v)])
+let v3_smul () = let v = V3.smul tvec3_1 2. in Alcotest.(check (list float)) "float list" [6.; 2.; 4.] ([(V3.x v); (V3.y v); (V3.z v)])
 let v3_op_smul () = todo ()
 let v3_opposite () = Alcotest.(check bool) "same bool" true (V3.opposite tvec3_1 tvec3_1opposite)
 let v3_opposite_failure () = Alcotest.(check bool) "same bool" false (V3.opposite tvec3_1 tvec3_2)
-let v3_dotproduct () = todo ()
-let v3_crossproduct () = todo ()
-let v3_magnitude () = todo ()
-let v3_magnitude2 () = todo ()
+(* TODO: Find out why this is failing, despite outputting identical-looking floats *)
+let v3_dotproduct () = Alcotest.(check float) "same float" 14.53249 (V3.dotproduct tvec3_1 tvec3_2)
+let v3_dotproduct2 () = Alcotest.(check float) "same float" 14. (V3.dotproduct tvec3_1 tvec3_1)
+(* TODO: Implement a second test using tvec3_1 and tvec2_3 *)
+(* crosstproduct = < a2b3 - a3b2, a3b1 - a1b3, a1b2 - a2b1 > *)
+let v3_crossproduct () = let v = V3.crossproduct tvec3_1 tvec3_1 in Alcotest.(check (list float)) "float list" [0.; 0.; 0.] ([(V3.x v); (V3.y v); (V3.z v)])
+(* FIXME: Precision error here, different between OCaml and gnome-calculator *)
+let v3_magnitude () = Alcotest.(check float) "same float" 3.741657387 (V3.magnitude tvec3_1)
+let v3_magnitude2 () = Alcotest.(check float) "same float" 14. (V3.magnitude2 tvec3_1)
 let v3_normalize () = todo ()
 let v3_distance () = todo ()
 
+(* This is how we do multiple tests per function. Note the semicolons! *)
 let v3_laborious_floats () =
   Alcotest.(check float) "same float" e (V3.x tvec3_2);
   Alcotest.(check float) "same float" pi (V3.y tvec3_2);
@@ -73,7 +80,8 @@ let () =
       "v3 neg",                  `Quick, v3_neg;
       "v3 add",                  `Quick, v3_add;
       (*"v3 op_add",               `Quick, v3_op_add;*)
-      (*"v3 sub",                  `Quick, v3_sub;*)
+      "v3 sub",                  `Quick, v3_sub;
+      "v3 sub2",                 `Quick, v3_sub2;
       "v3 op_sub",               `Quick, v3_op_sub;
       "v3 eq",                   `Quick, v3_eq;
       "v3 op_eq",                `Quick, v3_op_eq;
@@ -83,6 +91,7 @@ let () =
       "v3 opposite",             `Quick, v3_opposite;
       "v3 opposite failure",     `Quick, v3_opposite_failure;
       "v3 dot product",          `Quick, v3_dotproduct;
+      "v3 dot product2",         `Quick, v3_dotproduct2;
       "v3 cross product",        `Quick, v3_crossproduct;
       "v3 magnitude",            `Quick, v3_magnitude;
       "v3 magnitude2",           `Quick, v3_magnitude2;
