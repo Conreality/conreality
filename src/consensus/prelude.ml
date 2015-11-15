667960d319d96d3c147f846b11ebbbc60ccb250b
Arto Bendiken <arto@bendiken.net>
2015-11-15 03:52:55 +0100

    Implemented basic functions for Prelude.Float.

--- src/consensus/prelude.ml
+++ src/consensus/prelude.ml
@@ -1,1 +1,44 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+module Float = struct
+  type t = float
+
+  let infinity = Pervasives.infinity
+
+  let neg_infinity = Pervasives.neg_infinity
+
+  let nan = Pervasives.nan
+
+  let max = Pervasives.max_float
+
+  let min = Pervasives.min_float
+
+  let epsilon = Pervasives.epsilon_float
+
+  let classify = Pervasives.classify_float
+
+  let is_normal f =
+    match classify f with
+    | FP_normal -> true
+    | _         -> false
+
+  let is_subnormal f =
+    match classify f with
+    | FP_subnormal -> true
+    | _            -> false
+
+  let is_zero f =
+    match classify f with
+    | FP_zero -> true
+    | _       -> false
+
+  let is_infinite f =
+    match classify f with
+    | FP_infinite -> true
+    | _           -> false
+
+  let is_nan f =
+    match classify f with
+    | FP_nan -> true
+    | _      -> false
+end
--- src/consensus/prelude.mli
+++ src/consensus/prelude.mli
@@ -1,1 +1,17 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+module Float : sig
+  type t = float
+  val infinity : float
+  val neg_infinity : float
+  val nan : float
+  val max : float
+  val min : float
+  val epsilon : float
+  val classify : float -> fpclass
+  val is_normal : float -> bool
+  val is_subnormal : float -> bool
+  val is_zero : float -> bool
+  val is_infinite : float -> bool
+  val is_nan : float -> bool
+end
