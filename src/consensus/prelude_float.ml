b2f0219cd4d6c76e5a31db4ab9bc8856084f94e0
Mike Gogulski <mike@gogulski.com>
2015-11-18 15:49:39 +0100

    prelude_float: label Pervasives aliases for Bisect to ignore them

--- src/consensus/prelude_float.ml
+++ src/consensus/prelude_float.ml
@@ -6,12 +6,14 @@ let consensus_epsilon = 1e-9
 type t = float
 
 (* floating-point constants from Pervasives *)
+(* BISECT-IGNORE-BEGIN *)
 let infinity          = Pervasives.infinity
 let neg_infinity      = Pervasives.neg_infinity
 let nan               = Pervasives.nan
 let max_float         = Pervasives.max_float
 let min_float         = Pervasives.min_float
 let epsilon           = Pervasives.epsilon_float
+(* BISECT-IGNORE-END *)
 
 (* constants from /usr/include/math.h *)
 let e                 = 2.7182818284590452354  (* e *)
@@ -65,6 +67,7 @@ let is_nan f =
   | _      -> false
 
 (* Floating-point operations *)
+(* BISECT-IGNORE-BEGIN *)
 let ( ~-. ) x         = Pervasives.( ~-. ) x
 let ( ~+. ) x         = Pervasives.( ~+. ) x
 let ( +. ) x y        = Pervasives.( +. ) x y
@@ -78,25 +81,32 @@ let sqrt x            = Pervasives.sqrt x
 let log x             = Pervasives.log x
 let log10 x           = Pervasives.log10 x
 let log1p x           = Pervasives.log1p x
+(* BISECT-IGNORE-END *)
 
 (* Trigonometric functions, inverses, hyperbolics and inverse hyperbolics *)
 (* XXX: Do we need bounds-checking for x on some of these? *)
+(* BISECT-IGNORE-BEGIN *)
 let sin x             = Pervasives.sin x
 let cos x             = Pervasives.cos x
 let tan x             = Pervasives.tan x
+(* BISECT-IGNORE-END *)
 let csc x             = 1. /. (sin x)
 let sec x             = 1. /. (cos x)
 let cot x             = (cos x) /. (sin x)
+(* BISECT-IGNORE-BEGIN *)
 let asin x            = Pervasives.asin x
 let acos x            = Pervasives.acos x
 let atan x            = Pervasives.atan x
 let atan2 x y         = Pervasives.atan2 x y
+(* BISECT-IGNORE-END *)
 let acsc x            = asin (1. /. x)
 let asec x            = acos (1. /. x)
 let acot x            = pi_over_2 -. (atan x)
+(* BISECT-IGNORE-BEGIN *)
 let sinh x            = Pervasives.sinh x
 let cosh x            = Pervasives.cosh x
 let tanh x            = Pervasives.tanh x
+(* BISECT-IGNORE-END *)
 let csch x            = 1. /. (sinh x)
 let sech x            = 1. /. (cosh x)
 let coth x            = 1. /. (tanh x)
@@ -106,8 +116,9 @@ let atanh x           = (log ((1. +. x) /. (1. -. x))) /. 2.
 let acsch x           = asinh (1. /. x)
 let asech x           = acosh (1. /. x)
 let acoth x           = (log ((1. -. x) /. (1. +. x))) /. 2.
-let hypot x y         = Pervasives.hypot x y
+let hypot x y         = Pervasives.hypot x y (* BISECT-IGNORE *)
 
+(* BISECT-IGNORE-BEGIN *)
 let ceil x            = Pervasives.ceil x
 let floor x           = Pervasives.floor x
 let abs_float x       = Pervasives.abs_float x
@@ -120,6 +131,7 @@ let modf x            = Pervasives.modf x
 let float i           = Pervasives.float i
 let float_of_int i    = Pervasives.float_of_int i
 let truncate x        = Pervasives.truncate x
+(* BISECT-IGNORE-END *)
 
 (* Floating-point comparisons *)
 let ( == ) x y = x = y
@@ -176,14 +188,15 @@ let max x y =
   if (x <. y) then y else x
 
 (* Conversions *)
-let int_of_float x = Pervasives.int_of_float x
+let int_of_float x = Pervasives.int_of_float x (* BISECT-IGNORE *)
 external format_float : string -> float -> string = "caml_format_float"
-let valid_float_lexem s = Pervasives.valid_float_lexem s
+let valid_float_lexem s = Pervasives.valid_float_lexem s (* BISECT-IGNORE *)
 (* TODO: Do we need to switch 12 to 9 here? *)
 let string_of_float x = valid_float_lexem (format_float "%.12g" x)
-let float_of_string s = Pervasives.float_of_string s
+let float_of_string s = Pervasives.float_of_string s (* BISECT-IGNORE *)
 
 (* Other stuff *)
+(* BISECT-IGNORE-BEGIN *)
 let print_float x = Pervasives.print_float x
 let prerr_float x = Pervasives.prerr_float x
 let read_float () = Pervasives.read_float ()
