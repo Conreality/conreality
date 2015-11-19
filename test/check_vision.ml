d931a94cd87d03977118d793d63e5c848dfb85ee
Arto Bendiken <arto@bendiken.net>
2015-11-20 00:39:48 +0100

    Defined a test suite skeleton for the Vision module.

--- test/check_vision.ml
+++ test/check_vision.ml
@@ -1,5 +1,20 @@
 (* This is free and unencumbered software released into the public domain. *)
 
+open Check_common
 open Consensus.Vision
 
-let () = hello ()
+(* C++ integration *)
+
+module FFI_test = struct
+  let hello () =
+    (Consensus.Vision.hello ()) (* TODO *)
+end
+
+(* Test suite definition *)
+
+let () =
+  Alcotest.run "Consensus.Vision test suite" [
+    "C++ glue", [
+      "hello", `Quick, FFI_test.hello;
+    ];
+  ]
