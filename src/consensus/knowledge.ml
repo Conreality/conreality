1ccbf2cea40179b95cd3c88865172bd4d0b2c528
Arto Bendiken <arto@bendiken.net>
2015-11-22 03:44:21 +0100

    Implemented Term.make_uri, Term.make_uuid, and Term.make_node.

--- src/consensus/knowledge.ml
+++ src/consensus/knowledge.ml
@@ -1,3 +1,16 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 open Prelude
+
+module Term = struct
+  type t =
+    | URI of string
+    | UUID of string
+    | Node of string
+
+  let make_uri string = URI string
+
+  let make_uuid string = UUID string
+
+  let make_node label = Node label
+end
