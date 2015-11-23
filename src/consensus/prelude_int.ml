9cf3f9c779fc70a32b37553ba3162591f334e8ca
Arto Bendiken <arto@bendiken.net>
2015-11-23 10:20:14 +0000

    Implemented Int.of_string and Int.to_string.

--- src/consensus/prelude_int.ml
+++ src/consensus/prelude_int.ml
@@ -1,1 +1,5 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+let of_string = Pervasives.int_of_string
+
+let to_string = Pervasives.string_of_int
--- src/consensus/prelude_int.mli
+++ src/consensus/prelude_int.mli
@@ -1,1 +1,4 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+val of_string : string -> int
+val to_string : int -> string
