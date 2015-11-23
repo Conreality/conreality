e116c4e23e744cacb02859bb3e965e085a169f57
Arto Bendiken <arto@bendiken.net>
2015-11-23 11:49:24 +0000

    Defined the Consensus.Machinery module.

---
+++ test/check_machinery.ml
@@ -0,0 +1,11 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Check_common
+open Consensus.Machinery
+
+(* Test suite definition *)
+
+let () =
+  Alcotest.run "Consensus.Machinery test suite" [
+    (* TODO *)
+  ]
