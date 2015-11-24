4e3095bd9716b686c11a0d271cda8a36a479e0fa
Arto Bendiken <arto@bendiken.net>
2015-11-24 17:14:27 +0000

    Implemented a Ruby script for preprocessing ppx_include sources.

---
+++ etc/script/preprocess.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# This script preprocesses any ppx_include-reliant *.{ml,mli} source files,
+# writing the expanded output to the corresponding _build/ path.
+require 'fileutils'
+Dir['src/**/*.{ml,mli}'].sort.each do |source_path|
+  source_dir  = File.dirname(source_path)
+  source_text = File.read(source_path)
+  if source_text.each_line.any? { |line| line.include?('%%include') }
+    build_text = source_text.gsub(/\[%%include "([^"]+)"\]/) do |match|
+      File.read(File.join(source_dir, $1))
+    end
+    build_path = File.join('_build', source_path)
+    FileUtils.mkdir_p(File.dirname(build_path))
+    File.open(build_path, 'w+') do |build_file|
+      build_file.write(build_text)
+    end
+  end
+end
