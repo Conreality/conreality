fd4fa771ffd9b17614d1749fc6c2d60ad9bc00b4
Arto Bendiken <arto@bendiken.net>
2015-11-29 22:06:13 +0000

    Defined the Consensus.Measures module.

---
+++ test/check_measures.ml
@@ -0,0 +1,11 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Check_common
+open Consensus.Measures
+
+(* Test suite definition *)
+
+let () =
+  Alcotest.run "Consensus.Measures test suite" [
+    (* TODO *)
+  ]
