209ee9efa03fd22b6f7c614bf643618a136c5c76
Arto Bendiken <arto@bendiken.net>
2015-11-30 05:11:57 +0000

    Defined the essential Object interface.

--- src/consensus/model/object.ml
+++ src/consensus/model/object.ml
@@ -1,3 +1,96 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 open Prelude
+open Measures
+
+module P3 = struct
+  include Geometry.P3
+
+  let is_zero p = (p = zero)
+  let is_nonzero p = not (is_zero p)
+end
+
+module V3 = struct
+  include Geometry.V3
+
+  let is_zero v = (v = zero)
+  let is_nonzero v = not (is_zero v)
+end
+
+module Q = struct
+  include Geometry.Q
+
+  let is_zero q = (q = zero)
+  let is_nonzero q = not (is_zero q)
+end
+
+class virtual interface (id : int) = object (self)
+  method id = id
+
+  method label : string option = None
+
+  method position =
+    Some P3.zero (* TODO *)
+
+  method orientation =
+    Some Q.zero (* TODO *)
+
+  method linear_velocity =
+    Some V3.zero (* TODO *)
+
+  method angular_velocity =
+    Some Q.zero (* TODO *)
+
+  method linear_acceleration =
+    Some V3.zero (* TODO *)
+
+  method mass =
+    Some (Mass (0., kilogram)) (* TODO *)
+
+  method shape : Shape.t option =
+    None (* TODO *)
+
+  method color : unit option =
+    None (* TODO *)
+
+  method inverse_mass =
+    match self#mass with
+      | None -> None
+      | Some mass -> Some (Quantity.inverse mass)
+
+  method is_located =
+    match self#position with
+      | None -> false (* unknown position *)
+      | Some _ -> true
+
+  method is_immovable =
+    match self#mass with
+      | None -> false (* unknown mass *)
+      | Some mass -> Float.is_infinite (Quantity.length mass)
+
+  method is_moving =
+    match self#linear_velocity with
+      | None -> false (* unknown velocity *)
+      | Some velocity -> V3.is_nonzero velocity
+
+  method is_rotating =
+    match self#angular_velocity with
+      | None -> false (* unknown velocity *)
+      | Some velocity -> Q.is_nonzero velocity
+
+  method is_accelerating =
+    match self#linear_acceleration with
+      | None -> false (* unknown acceleration *)
+      | Some acceleration -> V3.is_nonzero acceleration
+
+  method is_active =
+    self#is_moving (* TODO? *)
+
+  method is_inactive =
+    not self#is_active
+
+  method to_string : string =
+    failwith "Not implemented as yet" (* TODO *)
+end
+
+type t = interface
--- src/consensus/model/object.mli
+++ src/consensus/model/object.mli
@@ -1,1 +1,64 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+class virtual interface : int -> object
+  (** The unique object identifier. *)
+  method id : int
+
+  (** The human-readable label, if any, for the object. *)
+  method label : string option
+
+  (** The current observed position, if any, of this object. *)
+  method position : Geometry.P3.t option
+
+  (** The current observed orientation, if any, of this object. *)
+  method orientation : Geometry.Q.t option
+
+  (** The current estimated linear velocity, if any, of this object. *)
+  method linear_velocity : Geometry.V3.t option
+
+  (** The current estimated angular velocity, if any, of this object. *)
+  method angular_velocity : Geometry.Q.t option
+
+  (** The current estimated linear acceleration, if any, of this object. *)
+  method linear_acceleration : Geometry.V3.t option
+
+  (** The estimated mass, if any, of this object. *)
+  method mass : Measures.Quantity.t option
+
+  (** The approximated shape, if any, of this object. *)
+  method shape : Shape.t option
+
+  (** The approximated color, if any, of this object. *)
+  method color : unit option
+
+  (** Computes the inverse mass of this object.
+      Immovable objects have zero inverse mass. *)
+  method inverse_mass : Measures.Quantity.t option
+
+  (** Determines whether this object has a nonzero position. *)
+  method is_located : bool
+
+  (** Determines whether this is an immovable physical object.
+      Objects with infinite mass are immovable. *)
+  method is_immovable : bool
+
+  (** Determines whether this object has a nonzero linear velocity. *)
+  method is_moving : bool
+
+  (** Determines whether this object has a nonzero angular velocity. *)
+  method is_rotating : bool
+
+  (** Determines whether this object has a nonzero linear acceleration. *)
+  method is_accelerating : bool
+
+  (** Determines whether this object is currently active. *)
+  method is_active : bool
+
+  (** Determines whether this object is currently inactive. *)
+  method is_inactive : bool
+
+  (** Returns a human-readable string representation of this object. *)
+  method to_string : string
+end
+
+type t = interface
