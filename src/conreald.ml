166ee17d6c2996b338e7309ef47af1b0080a55ff
Arto Bendiken <arto@bendiken.net>
2015-12-20 07:45:17 +0000

    Began implementing a UDP protocol handler.

--- src/conreald.ml
+++ src/conreald.ml
@@ -39,66 +39,79 @@ let verbosity_str = function
 
 (* Command implementations *)
 
-let execute_script script =
-  let context = Scripting.Context.create () in
-  Scripting.Context.eval_file context script
+module Experiments = struct
+  let connect addr port =
+    let sockfd = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
+    let sockaddr = Lwt_unix.ADDR_INET (addr, port) in
+    Lwt_unix.connect sockfd sockaddr
+    >>= fun () -> Lwt.return (sockfd)
 
-let connect addr port =
-  let sockfd = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
-  let sockaddr = Lwt_unix.ADDR_INET (addr, port) in
-  Lwt_unix.connect sockfd sockaddr
-  >>= fun () -> Lwt.return (sockfd)
+  let send sockfd message =
+    let channel = Lwt_io.of_fd sockfd ~mode:Lwt_io.output in
+    Lwt_io.write channel message
+    >>= fun () -> Lwt_io.flush channel
 
-let send sockfd message =
-  let channel = Lwt_io.of_fd sockfd ~mode:Lwt_io.output in
-  Lwt_io.write channel message
-  >>= fun () -> Lwt_io.flush channel
+  let recv_line sockfd =
+    let channel = Lwt_io.of_fd sockfd ~mode:Lwt_io.input in
+    Lwt_io.read_line channel
 
-let recv_line sockfd =
-  let channel = Lwt_io.of_fd sockfd ~mode:Lwt_io.input in
-  Lwt_io.read_line channel
+  let hello sockfd =
+    let frame = STOMP.Protocol.make_connect_frame "localhost" "admin" "password" in
+    send sockfd (STOMP.Frame.to_string frame)
+    >>= fun () -> recv_line sockfd
+    >>= fun (line) -> Printf.printf "%s\n" line; Lwt.return ()
+    >>= fun () -> Lwt.return ()
 
-let hello sockfd =
-  let frame = STOMP.Protocol.make_connect_frame "localhost" "admin" "password" in
-  send sockfd (STOMP.Frame.to_string frame)
-  >>= fun () -> recv_line sockfd
-  >>= fun (line) -> Printf.printf "%s\n" line; Lwt.return ()
-  >>= fun () -> Lwt.return ()
-
-let recv_udp_packet socket =
-  let buffer = (UDP.Packet.make_buffer ()) in
-  let rec loop () =
-    UDP.Socket.recvfrom socket buffer >>= fun (len, sa) ->
-    Lwt_log.ign_warning_f "Received %d bytes from %s..." len "?"; (* TODO *)
-    loop ()
-  in loop ()
-
-let run () =
-  Lwt_log.default := Lwt_log.syslog
-    ~facility:`Daemon
-    ~template:"$(name)[$(pid)]: $(message)" ();
-  Lwt_engine.on_timer 60. true (fun _ ->
-    Lwt_log.ign_info "Processed no requests in the last minute.") |> ignore; (* TODO *)
-  Lwt_unix.on_signal Sys.sigint (fun _ -> Lwt_unix.cancel_jobs (); exit 0) |> ignore;
-  Lwt_main.at_exit (fun () -> Lwt_log.notice "Shutting down...");
-  Lwt_log.ign_notice "Starting up...";
-  let udp_socket = UDP.Socket.bind "127.0.0.1" 1984 in
-  Lwt.async (fun () -> recv_udp_packet udp_socket);
-  fst (Lwt.wait ())
 (*
-  Lwt_unix.gethostbyname broker_name
-  >>= fun host -> begin
-    Lwt_log.ign_notice_f "Connecting to message broker at %s:%d..." host.h_name broker_port;
-    connect (Array.get host.h_addr_list 0) broker_port
-  end
-  >>= fun (sockfd) -> hello sockfd
-  >>= fun () -> loop ()
+  let run server =
+    ...
+    Lwt_unix.gethostbyname broker_name
+    >>= fun host -> begin
+      Lwt_log.ign_notice_f "Connecting to message broker at %s:%d..." host.h_name broker_port;
+      connect (Array.get host.h_addr_list 0) broker_port
+    end
+    >>= fun (sockfd) -> hello sockfd
+    >>= fun () -> loop ()
 *)
+end
+
+module Server = struct
+  type t = { context: Scripting.Context.t; clients: string list }
+
+  let create () =
+    { context = Scripting.Context.create (); clients = [] }
+
+  let evaluate server script =
+    Scripting.Context.eval_code server.context script (* TODO: error handling *)
+
+  let recv_command socket callback =
+    let buffer = (UDP.Packet.make_buffer ()) in
+    let rec loop () =
+      UDP.Socket.recvfrom socket buffer >>= fun (len, sa) ->
+      let command = String.sub (Lwt_bytes.to_string buffer) 0 len in
+      Lwt_log.ign_notice_f "Received %d bytes from %s: %s" len "?" command; (* TODO *)
+      callback (if (String.length command) > 1 then command else ""); (* for `nc` probe packets *)
+      loop ()
+    in loop ()
+
+  let run server =
+    Lwt_log.default := Lwt_log.syslog
+      ~facility:`Daemon
+      ~template:"$(name)[$(pid)]: $(message)" ();
+    Lwt_engine.on_timer 60. true (fun _ ->
+      Lwt_log.ign_info "Processed no requests in the last minute.") |> ignore; (* TODO *)
+    Lwt_unix.on_signal Sys.sigint (fun _ -> Lwt_unix.cancel_jobs (); exit 0) |> ignore;
+    Lwt_main.at_exit (fun () -> Lwt_log.notice "Shutting down...");
+    Lwt_log.ign_notice "Starting up...";
+    let udp_socket = UDP.Socket.bind "127.0.0.1" 1984 in
+    Lwt.async (fun () -> recv_command udp_socket (evaluate server));
+    fst (Lwt.wait ())
+end
 
 let main options mission =
   if String.is_empty mission
   then `Error (true, "no mission scenario script specified")
-  else `Ok (Lwt_main.run (run ()))
+  else `Ok (Lwt_main.run (Server.run (Server.create ())))
 
 (* Options common to all commands *)
 
