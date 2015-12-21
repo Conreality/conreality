e4c58a3408e43a549754786bda58dec1d4b5aadf
Arto Bendiken <arto@bendiken.net>
2015-12-21 11:16:10 +0000

    Defined the Consensus.Syntax module.

---
+++ test/check_syntax.ml
@@ -0,0 +1,19 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Check_common
+open Consensus.Syntax
+
+(* Syntax *)
+
+module Syntax_test = struct
+  (* TODO *)
+end
+
+(* Test suite definition *)
+
+let () =
+  Alcotest.run "Consensus.Syntax test suite" [
+    "Syntax", [
+      (* TODO *)
+    ];
+  ]
