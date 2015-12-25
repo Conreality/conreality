6d920f800beae000a252c0d44a6e1a4bea308794
Arto Bendiken <arto@bendiken.net>
2015-12-25 18:11:55 +0000

    Defined the Consensus.Config module.

---
+++ test/check_config.ml
@@ -0,0 +1,19 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Check_common
+open Consensus.Config
+
+(* Config *)
+
+module Config_test = struct
+  (* TODO *)
+end
+
+(* Test suite definition *)
+
+let () =
+  Alcotest.run "Consensus.Config test suite" [
+    "Config", [
+      (* TODO *)
+    ];
+  ]
