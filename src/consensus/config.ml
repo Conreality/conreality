5990c0b0c29ce58aab6c54964f6fcc06c55db338
Arto Bendiken <arto@bendiken.net>
2015-12-25 18:48:25 +0000

    Defined Config.t and implemented Config.load_file.

--- src/consensus/config.ml
+++ src/consensus/config.ml
@@ -2,3 +2,50 @@
 
 open Prelude
 open Scripting
+
+module Devices = struct
+  type t = unit
+
+  let create () = () (* TODO *)
+
+  let load devices context = () (* TODO *)
+end
+
+module Network = struct
+  type t = {
+    irc: Table.t;
+    ros: Table.t;
+    stomp: Table.t;
+  }
+
+  let create () = {
+    irc   = Table.create 0;
+    ros   = Table.create 0;
+    stomp = Table.create 0;
+  }
+
+  let load network context = ()
+end
+
+type t = {
+  devices: Devices.t;
+  network: Network.t;
+}
+
+let create () =
+  {
+    devices = Devices.create ();
+    network = Network.create ();
+  }
+
+let load_file pathname =
+  let config = create () in
+  let context = Scripting.Context.create () in
+  Scripting.Context.define context "devices"
+    (fun context ->
+      Devices.load config.devices context |> ignore; 0);
+  Scripting.Context.define context "network"
+    (fun context ->
+      Network.load config.network context |> ignore; 0);
+  Scripting.Context.eval_file context pathname;
+  config
--- src/consensus/config.mli
+++ src/consensus/config.mli
@@ -1,3 +1,15 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 (** Configuration. *)
+
+module Devices : sig
+  type t
+end
+
+module Network : sig
+  type t
+end
+
+type t
+
+val load_file : string -> t
