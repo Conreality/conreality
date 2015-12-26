a5597a015b553ce9bbca327a3c34e5e6325825cc
Arto Bendiken <arto@bendiken.net>
2015-12-26 20:44:32 +0000

    Implemented Syntax.Token.to_string.

--- src/consensus/syntax/token.ml
+++ src/consensus/syntax/token.ml
@@ -1,32 +1,32 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 type t =
-  | UP
-  | TRACK
-  | TOGGLE
-  | TO
-  | TILT
-  | SYMBOL of string
-  | SECONDS
-  | RIGHT
-  | RESUME
-  | RADIANS
-  | PING
-  | PAN
-  | OCLOCK
-  | LEFT
-  | LEAVE
-  | JOIN
-  | INTEGER of int
-  | HOLD
-  | FLOAT of float
-  | FIRE
-  | EOF
-  | ENABLE
-  | DOWN
-  | DISABLE
-  | DEGREES
   | ABORT
+  | DEGREES
+  | DISABLE
+  | DOWN
+  | ENABLE
+  | EOF
+  | FIRE
+  | FLOAT of float
+  | HOLD
+  | INTEGER of int
+  | JOIN
+  | LEAVE
+  | LEFT
+  | OCLOCK
+  | PAN
+  | PING
+  | RADIANS
+  | RESUME
+  | RIGHT
+  | SECONDS
+  | SYMBOL of string
+  | TILT
+  | TO
+  | TOGGLE
+  | TRACK
+  | UP
 
 type token = t
 
@@ -34,3 +34,31 @@ let is_verb = function
   | ABORT | DISABLE | ENABLE | FIRE | HOLD | JOIN | LEAVE
   | PAN | PING | RESUME | TILT | TOGGLE | TRACK -> true
   | _ -> false
+
+let to_string  = function
+  | ABORT     -> "abort"
+  | DEGREES   -> "degrees"
+  | DISABLE   -> "disable"
+  | DOWN      -> "down"
+  | ENABLE    -> "enable"
+  | EOF       -> ""
+  | FIRE      -> "fire"
+  | FLOAT f   -> Printf.sprintf "%f" f
+  | HOLD      -> "hold"
+  | INTEGER n -> Printf.sprintf "%d" n
+  | JOIN      -> "join"
+  | LEAVE     -> "leave"
+  | LEFT      -> "left"
+  | OCLOCK    -> "o'clock"
+  | PAN       -> "pan"
+  | PING      -> "ping"
+  | RADIANS   -> "radians"
+  | RESUME    -> "resume"
+  | RIGHT     -> "right"
+  | SECONDS   -> "seconds"
+  | SYMBOL s  -> s
+  | TILT      -> "tilt"
+  | TO        -> "to"
+  | TOGGLE    -> "toggle"
+  | TRACK     -> "track"
+  | UP        -> "up"
