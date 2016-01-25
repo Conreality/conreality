c402f4422a1b3142dc59ef63ee2fadee36a9a2eb
Arto Bendiken <arto@bendiken.net>
2016-01-25 08:05:09 +0000

    Made the Bytes module backwards compatible with OCaml 4.01.

--- src/consensus/prelude/bytes.ml
+++ src/consensus/prelude/bytes.ml
@@ -1,14 +1,51 @@
 (* This is free and unencumbered software released into the public domain. *)
 
-include Bytes
+#if OCAMLVERSION < 40200
+type bytes = string
+#endif
 
-let is_empty bytes = (Bytes.length bytes) = 0
+#if OCAMLVERSION < 40200
+include String
+#else
+include Bytes
+#endif
+
+#if OCAMLVERSION < 40200
+let empty = ""
+#endif
+
+#if OCAMLVERSION < 40200
+let of_string = String.copy
+#endif
+
+#if OCAMLVERSION < 40200
+let to_string = String.copy
+#endif
+
+#if OCAMLVERSION < 40200
+let sub_string = sub
+#endif
+
+#if OCAMLVERSION < 40200
+let extend (s : bytes) (left : int) (right : int) =
+  failwith "not implemented for OCaml 4.01.0" (* TODO *)
+#endif
+
+#if OCAMLVERSION < 40200
+let blit_string = blit
+#endif
+
+#if OCAMLVERSION < 40200
+let cat s1 s2 = s1 ^ s2
+#endif
+
+let is_empty bytes = (length bytes) = 0
 
 let of_bool value =
-  Bytes.of_string (Pervasives.string_of_bool value) (*BISECT-IGNORE*)
+  of_string (Pervasives.string_of_bool value) (*BISECT-IGNORE*)
 
 let of_float value =
-  Bytes.of_string (Pervasives.string_of_float value) (*BISECT-IGNORE*)
+  of_string (Pervasives.string_of_float value) (*BISECT-IGNORE*)
 
 let of_int value =
-  Bytes.of_string (Pervasives.string_of_int value) (*BISECT-IGNORE*)
+  of_string (Pervasives.string_of_int value) (*BISECT-IGNORE*)
--- src/consensus/prelude/bytes.mli
+++ src/consensus/prelude/bytes.mli
@@ -1,11 +1,17 @@
 (* This is free and unencumbered software released into the public domain. *)
 
+#if OCAMLVERSION < 40200
+type bytes = string
+#endif
+
 external length : bytes -> int = "%string_length"
 external get : bytes -> int -> char = "%string_safe_get"
 external set : bytes -> int -> char -> unit = "%string_safe_set"
 external create : int -> bytes = "caml_create_string"
 val make : int -> char -> bytes
+#if OCAMLVERSION >= 40200
 val init : int -> (int -> char) -> bytes
+#endif
 val empty : bytes
 val copy : bytes -> bytes
 val of_string : string -> bytes
@@ -21,7 +27,9 @@ val cat : bytes -> bytes -> bytes
 val iter : (char -> unit) -> bytes -> unit
 val iteri : (int -> char -> unit) -> bytes -> unit
 val map : (char -> char) -> bytes -> bytes
+#if OCAMLVERSION >= 40200
 val mapi : (int -> char -> char) -> bytes -> bytes
+#endif
 val trim : bytes -> bytes
 val escaped : bytes -> bytes
 val index : bytes -> char -> int
@@ -37,14 +45,17 @@ val capitalize : bytes -> bytes
 val uncapitalize : bytes -> bytes
 type t = bytes
 val compare : t -> t -> int
+#if OCAMLVERSION >= 40200
 val unsafe_to_string : bytes -> string
 val unsafe_of_string : string -> bytes
+#endif
 external unsafe_get : bytes -> int -> char = "%string_unsafe_get"
 external unsafe_set : bytes -> int -> char -> unit = "%string_unsafe_set"
 external unsafe_blit : bytes -> int -> bytes -> int -> int -> unit
   = "caml_blit_string" "noalloc"
 external unsafe_fill : bytes -> int -> int -> char -> unit
   = "caml_fill_string" "noalloc"
+
 val is_empty : bytes -> bool
 val of_bool : bool -> bytes
 val of_float : float -> bytes
