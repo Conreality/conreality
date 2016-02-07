e8e6d0c8be47f2ff6b350f286e6d36ec4fff8796
Arto Bendiken <arto@bendiken.net>
2016-02-07 06:19:32 +0100

    Implemented Char.{of,to}_int{,32,64}.

--- src/consensus/prelude/char.ml
+++ src/consensus/prelude/char.ml
@@ -2,6 +2,18 @@
 
 include Char
 
+let of_int = chr
+
+let to_int = code
+
+let of_int32 n = of_int (Int32.to_int n)
+
+let to_int32 c = Int32.of_int (to_int c)
+
+let of_int64 n = of_int (Int64.to_int n)
+
+let to_int64 c = Int64.of_int (to_int c)
+
 let of_string s = String.get s 0
 
 let to_string c = String.make 1 c
--- src/consensus/prelude/char.mli
+++ src/consensus/prelude/char.mli
@@ -8,6 +8,13 @@ val uppercase : char -> char
 type t = char
 val compare : t -> t -> int
 external unsafe_chr : int -> char = "%identity"
+
+val of_int : int -> char
+val to_int : char -> int
+val of_int32 : int32 -> char
+val to_int32 : char -> int32
+val of_int64 : int64 -> char
+val to_int64 : char -> int64
 val of_string : string -> char
 val to_string : char -> string
 val compare : t -> t -> int
