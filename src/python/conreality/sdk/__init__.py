0fe915f40d260e06d71fb0f7a7f5bc4bd26ec8b0
Arto Bendiken <arto@bendiken.net>
2016-02-28 20:48:11 +0100

    Defined conreality.sdk.model.Object.

--- src/python/conreality/sdk/__init__.py
+++ src/python/conreality/sdk/__init__.py
@@ -1,3 +1,8 @@
 # This is free and unencumbered software released into the public domain.
 
 __all__ = ['measures', 'messaging', 'model', 'scripting']
+
+from . import measures
+from . import messaging
+from . import model
+from . import scripting
--- src/python/conreality/sdk/model.py
+++ src/python/conreality/sdk/model.py
@@ -1,1 +1,64 @@
 # This is free and unencumbered software released into the public domain.
+
+"""World model."""
+
+class ObjectColor(object):
+  pass
+
+class ObjectShape(object):
+  pass
+
+class Object(object):
+  """An object of interest."""
+
+  def __init__(self, id=None):
+    self.id = id
+    self.label = None
+    self.position = None
+    self.orientation = None
+    self.linear_velocity = None
+    self.angular_velocity = None
+    self.linear_acceleration = None
+    self.mass = None
+    self.shape = None
+    self.color = None
+
+  def inverse_mass(self):
+    """Computes the inverse mass of this object."""
+    return None # unknown
+
+  def is_located(self):
+    """Determines whether this object has a nonzero position."""
+    return False
+
+  def is_immovable(self):
+    """Determines whether this is an immovable physical object."""
+    return None # unknown
+
+  def is_moving(self):
+    """Determines whether this object has a nonzero linear velocity."""
+    return None # unknown
+
+  def is_rotating(self):
+    """Determines whether this object has a nonzero angular velocity."""
+    return None # unknown
+
+  def is_accelerating(self):
+    """Determines whether this object has a nonzero linear acceleration."""
+    return None # unknown
+
+  def is_active(self):
+    """Determines whether this object is currently active."""
+    return None # unknown
+
+  def is_inactive(self):
+    """Determines whether this object is currently inactive."""
+    return None # unknown
+
+  def __repr__(self):
+    """Returns a human-readable string representation of this object."""
+    return "object{}" # TODO
+
+  def __str__(self):
+    """Returns a human-readable string representation of this object."""
+    return self.__repr__()
