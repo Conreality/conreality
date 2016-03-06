4aa3e661f65ba4e679583f9234d0a291ecb88670
Arto Bendiken <arto@bendiken.net>
2016-03-07 00:11:43 +0100

    Implemented conreality.ddk.camera.CameraRegistry.

--- src/python/conreality/ddk/camera.py
+++ src/python/conreality/ddk/camera.py
@@ -6,6 +6,45 @@ from ..sdk.vision import SharedImage
 from .driver import DataDirectory
 import os
 
+DATA_PATH      = 'cameras'
+DEFAULT_WIDTH  = 640
+DEFAULT_HEIGHT = 480
+
+class CameraRegistry(DataDirectory):
+  def __init__(self):
+    super(CameraRegistry, self).__init__(DATA_PATH)
+    self.refresh()
+
+  def refresh(self):
+    self.dirs = {}
+    for id in os.listdir(self.path):
+      if os.path.isdir(os.path.join(self.path, id)):
+        self.dirs[id] = CameraDirectory(id)
+
+  def __len__(self):
+    return len(self.dirs)
+
+  def __iter__(self):
+    return iter(self.dirs.values())
+
+  def __contains__(self, id):
+    return id in self.dirs
+
+  def __getitem__(self, id):
+    return self.dirs[id]
+
+class CameraDirectory(DataDirectory):
+  def __init__(self, id):
+    super(CameraDirectory, self).__init__(DATA_PATH, id)
+    self.id = id
+
+  def open_feed(self, **kwargs):
+    if not 'mode' in kwargs:
+      kwargs['mode'] = self.mode or 'r'
+    if not 'width' in kwargs or not 'height' in kwargs:
+      pass # TODO
+    return CameraFeed(self, **kwargs)
+
 class CameraFeed(object):
   def __init__(self, dir, width, height, format='bgr', mode='r'):
     self.path = os.path.join(dir.path, '{}x{}.{}'.format(width, height, format))
@@ -13,15 +52,3 @@ class CameraFeed(object):
 
   def snap(self):
     return self.image.copy()
-
-class CameraDirectory(DataDirectory):
-  def __init__(self, id):
-    super(CameraDirectory, self).__init__('cameras', id)
-
-  def open_feed(self, **kwargs):
-    if not 'mode' in kwargs:
-      kwargs['mode'] = self.mode or 'r'
-    return CameraFeed(self, **kwargs)
-
-class CameraRegistry(object):
-  pass # TODO
