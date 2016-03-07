dfbc021ddd75e5e22800650f7891a52972a84636
Arto Bendiken <arto@bendiken.net>
2016-03-08 00:15:06 +0100

    Implemented conreality.sdk.video.VideoEncoder.

--- src/python/conreality/sdk/video.py
+++ src/python/conreality/sdk/video.py
@@ -3,3 +3,22 @@
 """Video input/output."""
 
 import cv2
+
+class VideoEncoder(object):
+  """Video encoder for writing MPEG-4 video files."""
+
+  def __init__(self, filepath, size, fps=20, fourcc='FMP4'):
+    self.fourcc = cv2.VideoWriter_fourcc(*fourcc)
+    self.writer = cv2.VideoWriter(filepath, self.fourcc, fps, size, True)
+
+  def is_open(self):
+    return self.writer.isOpened()
+
+  def is_closed(self):
+    return not self.is_open()
+
+  def close(self):
+    self.writer.release()
+
+  def write(self, frame):
+    self.writer.write(frame.data)
