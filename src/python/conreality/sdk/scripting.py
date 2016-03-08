01ac844b8d7ffa7502780795d79186d15281bf5b
Arto Bendiken <arto@bendiken.net>
2016-03-08 02:10:07 +0100

    Implemented conreality.sdk.scripting.Context.

--- src/python/conreality/sdk/scripting.py
+++ src/python/conreality/sdk/scripting.py
@@ -1,1 +1,26 @@
 # This is free and unencumbered software released into the public domain.
+
+import lupa
+
+class Context(object):
+  def __init__(self):
+    self.runtime = lupa.LuaRuntime(unpack_returned_tuples=True)
+
+  def load_code(self, code):
+    self.eval_code(code)
+
+  def load_file(self, filepath):
+    self.eval_file(filepath)
+
+  def eval_code(self, code):
+    return self.runtime.eval(code)
+
+  def eval_file(self, filepath):
+    with open(filepath, 'r') as file:
+      return self.runtime.eval(file.read())
+
+  def define(self, name, function):
+    self.runtime.globals()[name] = function
+
+  def undefine(self, name):
+    self.runtime.globals()[name] = None
