0f9dcd15c42967cfeaff901a898d4f24229b8cc8
Arto Bendiken <arto@bendiken.net>
2016-03-19 17:49:07 +0100

    Added test cases for conreality.sdk.vision.Image.

--- test/python/vision.py
+++ test/python/vision.py
@@ -6,6 +6,8 @@
 from conreality.sdk.vision import *
 
 class TestColor:
+  """Test cases for the conreality.sdk.vision.Color class."""
+
   def test_positional_construction(self):
     color = Color(1, 2, 3)
     assert color.r == 1
@@ -35,15 +37,41 @@ class TestColor:
     assert str(color) == "color{r=1, g=2, b=3}"
 
 class TestImage:
-  def test(self):
-    assert True # TODO
+  """Test cases for the conreality.sdk.vision.Image class."""
+
+  def test_construction_from_data(self):
+    image_data = numpy.zeros((240, 320, 3), numpy.uint8)
+    image = Image(data=image_data, format='bgr')
+    assert image.width == 320
+    assert image.height == 240
+    assert image.is_bgr() # default format
+    assert image.data is not None
+
+  def test_construction_from_width_and_height(self):
+    image = Image(width=320, height=240)
+    assert image.width == 320
+    assert image.height == 240
+    assert image.is_bgr() # default format
+    assert image.data is not None
+
+  def test_dimensions_method(self):
+    image = Image(width=320, height=240)
+    assert image.dimensions() == (240, 320, 3)
+
+  def test_to_bgr_method(self):
+    image = Image(width=320, height=240)
+    #assert image.to_bgr().is_bgr() # TODO: ImportError('cv2') on Ubuntu 14.04
 
 class TestSharedImage:
-  def test(self):
+  """Test cases for the conreality.sdk.vision.SharedImage class."""
+
+  def test_construction(self):
     assert True # TODO
 
 class TestCascadeClassifier:
-  def test(self):
+  """Test cases for the conreality.sdk.vision.CascadeClassifier class."""
+
+  def test_construction(self):
     assert True # TODO
 
 if __name__ == '__main__':
