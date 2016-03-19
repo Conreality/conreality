75f6d8606835858263f46114409dae4e79df0d73
Arto Bendiken <arto@bendiken.net>
2016-03-19 17:31:37 +0100

    Added test cases for conreality.sdk.vision.Color.

--- test/python/vision.py
+++ test/python/vision.py
@@ -6,8 +6,33 @@
 from conreality.sdk.vision import *
 
 class TestColor:
-  def test(self):
-    assert True # TODO
+  def test_positional_construction(self):
+    color = Color(1, 2, 3)
+    assert color.r == 1
+    assert color.g == 2
+    assert color.b == 3
+
+  def test_keyword_construction(self):
+    color = Color(r=1, g=2, b=3)
+    assert color.r == 1
+    assert color.g == 2
+    assert color.b == 3
+
+  def test_rgb_method(self):
+    color = Color(1, 2, 3)
+    assert color.rgb() == (1, 2, 3)
+
+  def test_bgr_method(self):
+    color = Color(1, 2, 3)
+    assert color.bgr() == (3, 2, 1)
+
+  def test_repr_result(self):
+    color = Color(1, 2, 3)
+    assert repr(color) == "Color(r=1, g=2, b=3)"
+
+  def test_str_result(self):
+    color = Color(1, 2, 3)
+    assert str(color) == "color{r=1, g=2, b=3}"
 
 class TestImage:
   def test(self):
