cd867eea5a555f7fc4a79fc7909678c70a6d97dd
Arto Bendiken <arto@bendiken.net>
2016-03-21 05:33:05 +0100

    Defined conreality.sdk.messaging classes.

--- src/python/conreality/sdk/messaging.py
+++ src/python/conreality/sdk/messaging.py
@@ -1,1 +1,63 @@
 # This is free and unencumbered software released into the public domain.
+
+"""Messaging protocols."""
+
+from .storage import DataDirectory
+import os
+
+DATA_PATH = 'topics'
+
+class TopicRegistry(DataDirectory):
+  """Topic registry."""
+
+  def __init__(self, path=None):
+    if path:
+      self.path = path
+    else:
+      super(TopicRegistry, self).__init__(DATA_PATH)
+
+  def topics(self):
+    for entry in self.scan():
+      if not entry.name.startswith('.') and entry.is_dir():
+        yield Topic(path=os.path.join(self.path, entry.name))
+
+class Topic(DataDirectory):
+  """Topic exchange."""
+
+  def __init__(self, path=None, name=None):
+    if name:
+      self.name = name
+      super(Topic, self).__init__(DATA_PATH, name)
+    elif path:
+      self.name = os.path.basename(path)
+      self.path = path
+    else:
+      raise ValueError("no topic name or path specified")
+    self.open(mode='r+')
+
+  def publish(self, message):
+    return Publisher(self).send(message)
+
+  def subscribe(self):
+    return Subscriber(self)
+
+class Publisher:
+  """Message publisher."""
+
+  def __init__(self, topic):
+    self.topic = topic
+
+  def send(self, message):
+    pass # TODO
+
+class Subscriber:
+  """Message subscriber."""
+
+  def __init__(self, topic):
+    self.topic = topic
+
+  def subscribe(self):
+    pass # TODO
+
+  def unsubscribe(self):
+    pass # TODO
--- test/python/messaging.py
+++ test/python/messaging.py
@@ -4,12 +4,49 @@
 """Test cases for the conreality.sdk.messaging module."""
 
 from conreality.sdk.messaging import *
+from conreality.sdk.storage import DataDirectory
+from inspect import *
+import os
 
-class Test: # TODO
-  """Test cases for the conreality.sdk.messaging. class.""" # TODO
+class TestTopicRegistry:
+  """Test cases for the conreality.sdk.messaging.TopicRegistry class."""
+
+  def test_construction_from_path(self):
+    registry = TopicRegistry(path='/tmp')
+    assert registry.path == '/tmp'
+
+  def test_topics_generator(self):
+    registry = TopicRegistry(path='/tmp')
+    assert isgenerator(registry.topics())
+
+class TestTopic:
+  """Test cases for the conreality.sdk.messaging.Topic class."""
+
+  def test_construction_from_path(self):
+    topic = Topic(path='/tmp/foobar')
+    assert topic.name == 'foobar'
+    assert topic.path == '/tmp/foobar'
+    assert topic.mode == 'r+'
+
+  def test_construction_from_name(self):
+    topic = Topic(name='foobar')
+    assert topic.name == 'foobar'
+    assert topic.path == os.path.join(DataDirectory.BASE_PATH, 'topics', 'foobar')
+    assert topic.mode == 'r+'
+
+class TestPublisher:
+  """Test cases for the conreality.sdk.messaging.Publisher class."""
 
   def test_construction(self):
-    assert True # TODO
+    publisher = Publisher(Topic(name='foobar'))
+    assert publisher # TODO
+
+class TestSubscriber:
+  """Test cases for the conreality.sdk.messaging.Subscriber class."""
+
+  def test_construction(self):
+    subscriber = Subscriber(Topic(name='foobar'))
+    assert subscriber # TODO
 
 if __name__ == '__main__':
   import pytest, sys
