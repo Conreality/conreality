6eec423d32734502c6b3855e3f58bc6eb8b470b7
Arto Bendiken <arto@bendiken.net>
2016-03-25 00:42:12 +0100

    Enhanced conreality.sdk.messaging tests using tmpdir fixtures.

--- test/python/messaging.py
+++ test/python/messaging.py
@@ -11,28 +11,33 @@ import os
 class TestTopicRegistry:
   """Test cases for the conreality.sdk.messaging.TopicRegistry class."""
 
-  def test_construction_from_path(self):
-    registry = TopicRegistry(path='/tmp')
-    assert registry.path == '/tmp'
+  def test_construction_from_path(self, tmpdir):
+    registry = TopicRegistry(path=str(tmpdir))
+    assert registry.path == str(tmpdir)
 
-  def test_topics_generator(self):
-    registry = TopicRegistry(path='/tmp')
+  def test_topics_generator(self, tmpdir):
+    registry = TopicRegistry(path=str(tmpdir))
     assert isgenerator(registry.topics())
 
 class TestTopic:
   """Test cases for the conreality.sdk.messaging.Topic class."""
 
-  def test_construction_from_path(self):
-    topic = Topic(path='/tmp/foobar')
+  def test_construction_from_path(self, tmpdir):
+    topic = Topic(path=str(tmpdir.join('foobar')))
     assert topic.name == 'foobar'
-    assert topic.path == '/tmp/foobar'
+    assert topic.path == os.path.join(str(tmpdir), 'foobar')
     assert topic.mode == 'r+'
 
-  def test_construction_from_name(self):
+  def test_construction_from_name(self, tmpdir):
+    DataDirectory.BASE_PATH = str(tmpdir)
     topic = Topic(name='foobar')
     assert topic.name == 'foobar'
     assert topic.path == os.path.join(DataDirectory.BASE_PATH, 'topics', 'foobar')
     assert topic.mode == 'r+'
+
+  def test_subscribers_generator(self):
+    topic = Topic(name='foobar')
+    assert isgenerator(topic.subscribers())
 
 class TestPublisher:
   """Test cases for the conreality.sdk.messaging.Publisher class."""
