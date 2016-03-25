ec63172ec34edb08957c08fcf9b3c4ce137632c7
Arto Bendiken <arto@bendiken.net>
2016-03-25 03:43:44 +0100

    Implemented conreality.sdk.messaging.{Topic,Subscriber,Publisher}.

--- src/python/conreality/sdk/messaging.py
+++ src/python/conreality/sdk/messaging.py
@@ -3,7 +3,9 @@
 """Messaging protocols."""
 
 from .storage import DataDirectory
+from select import PIPE_BUF, select
 import os
+import threading
 
 DATA_PATH = 'topics'
 
@@ -12,11 +14,12 @@ class TopicRegistry(DataDirectory):
 
   def __init__(self, path=None):
     if path:
-      self.path = path
+      self.path = str(path)
     else:
-      super(TopicRegistry, self).__init__(DATA_PATH)
+      super().__init__(DATA_PATH)
 
   def topics(self):
+    """Yields each of the topics in this topic registry."""
     for entry in self.scan():
       if not entry.name.startswith('.') and entry.is_dir():
         yield Topic(path=os.path.join(self.path, entry.name))
@@ -27,37 +30,107 @@ class Topic(DataDirectory):
   def __init__(self, path=None, name=None):
     if name:
       self.name = name
-      super(Topic, self).__init__(DATA_PATH, name)
+      super().__init__(DATA_PATH, name)
     elif path:
-      self.name = os.path.basename(path)
-      self.path = path
+      self.path = str(path)
+      self.name = os.path.basename(self.path)
     else:
       raise ValueError("no topic name or path specified")
     self.open(mode='r+')
 
   def publish(self, message):
-    return Publisher(self).send(message)
+    """Publishes a message to subscribers of this topic."""
+    return Publisher(topic=self).publish(message)
 
   def subscribe(self):
-    return Subscriber(self)
+    """Creates a new subscriber for this topic."""
+    return Subscriber(topic=self)
 
-class Publisher:
-  """Message publisher."""
-
-  def __init__(self, topic):
-    self.topic = topic
-
-  def send(self, message):
-    pass # TODO
+  def subscribers(self, numeric=False):
+    """Yields each of the subscribers of this topic."""
+    for entry in self.scan():
+      if entry.name.isnumeric():
+        yield Subscriber(topic=self, id=int(entry.name))
 
 class Subscriber:
   """Message subscriber."""
 
-  def __init__(self, topic):
+  PIPE_MODE  = 0o666
+  PIPE_FLAGS = os.O_RDONLY | os.O_NONBLOCK | os.O_CLOEXEC
+
+  def __init__(self, topic, id=None):
     self.topic = topic
+    self.id = int(id) if id else threading.get_ident()
+    self.path = os.path.join(self.topic.path, str(self.id))
+    self.fd = None
 
-  def subscribe(self):
-    pass # TODO
+  def __enter__(self):
+    self.open()
+    return self
 
-  def unsubscribe(self):
-    pass # TODO
+  def __exit__(self, *args):
+    self.close()
+    return False
+
+  def open(self):
+    """Establishes the subscription to the subscriber topic."""
+    assert not self.fd
+    os.mkfifo(self.path, mode=self.PIPE_MODE)
+    self.fd = os.open(self.path, flags=self.PIPE_FLAGS)
+    return self
+
+  def close(self):
+    """Cancels the subscription to the subscriber topic."""
+    os.unlink(self.path)
+    if self.fd:
+      os.close(self.fd)
+      self.fd = None
+    return self
+
+  def receive(self, timeout=None):
+    """Returns the next message published to the subscriber topic."""
+    assert self.fd
+    select([self.fd], [], [], timeout) # FIXME: check return value
+    buffer = os.read(self.fd, PIPE_BUF)
+    message = buffer.decode() # TODO: Message(data=buffer)
+    return message
+
+class Publisher:
+  """Message publisher."""
+
+  PIPE_FLAGS = os.O_WRONLY | os.O_NONBLOCK | os.O_CLOEXEC
+
+  def __init__(self, topic, id=None):
+    self.topic = topic
+    self.id = int(id) if id else threading.get_ident()
+
+  def __enter__(self):
+    self.open()
+    return self
+
+  def __exit__(self, *args):
+    self.close()
+    return False
+
+  def open(self):
+    # TODO: open all subscriber pipes and watch for new subscribers.
+    return self
+
+  def close(self):
+    # TODO: close all subscriber pipes and cancel watch.
+    return self
+
+  def publish(self, message):
+    """Publishes a message to the publisher topic."""
+    buffer = message.encode() if type(message) is not bytes else message
+    if len(buffer) > PIPE_BUF:
+      raise ValueError("message size {} exceeds PIPE_BUF={} bytes".format(len(buffer), PIPE_BUF))
+    deliveries = set()
+    for subscriber in self.topic.subscribers():
+      fd = os.open(subscriber.path, flags=self.PIPE_FLAGS)
+      try:
+        os.write(fd, buffer)
+        deliveries.add(subscriber.id)
+      finally:
+        os.close(fd)
+    return deliveries
--- test/python/messaging.py
+++ test/python/messaging.py
@@ -3,27 +3,29 @@
 
 """Test cases for the conreality.sdk.messaging module."""
 
+from asyncio import Future
 from conreality.sdk.messaging import *
 from conreality.sdk.storage import DataDirectory
 from inspect import *
+from time import sleep
 import os
 
 class TestTopicRegistry:
   """Test cases for the conreality.sdk.messaging.TopicRegistry class."""
 
   def test_construction_from_path(self, tmpdir):
-    registry = TopicRegistry(path=str(tmpdir))
+    registry = TopicRegistry(path=tmpdir)
     assert registry.path == str(tmpdir)
 
   def test_topics_generator(self, tmpdir):
-    registry = TopicRegistry(path=str(tmpdir))
+    registry = TopicRegistry(path=tmpdir)
     assert isgenerator(registry.topics())
 
 class TestTopic:
   """Test cases for the conreality.sdk.messaging.Topic class."""
 
   def test_construction_from_path(self, tmpdir):
-    topic = Topic(path=str(tmpdir.join('foobar')))
+    topic = Topic(path=tmpdir.join('foobar'))
     assert topic.name == 'foobar'
     assert topic.path == os.path.join(str(tmpdir), 'foobar')
     assert topic.mode == 'r+'
@@ -39,19 +41,77 @@ class TestTopic:
     topic = Topic(name='foobar')
     assert isgenerator(topic.subscribers())
 
-class TestPublisher:
-  """Test cases for the conreality.sdk.messaging.Publisher class."""
-
-  def test_construction(self):
-    publisher = Publisher(Topic(name='foobar'))
-    assert publisher # TODO
-
 class TestSubscriber:
   """Test cases for the conreality.sdk.messaging.Subscriber class."""
 
-  def test_construction(self):
-    subscriber = Subscriber(Topic(name='foobar'))
-    assert subscriber # TODO
+  def test_construction(self, tmpdir):
+    topic = Topic(path=tmpdir.join('foobar'))
+    with Subscriber(topic) as subscriber:
+      assert subscriber.topic == topic
+      assert type(subscriber.id) is int
+      assert subscriber.path == str(tmpdir.join('foobar', str(subscriber.id)))
+      assert subscriber.fd is not None
+
+class TestPublisher:
+  """Test cases for the conreality.sdk.messaging.Publisher class."""
+
+  class SubscriberThread(threading.Thread):
+    def __init__(self, topic, id):
+      super().__init__(target=self)
+      self.subscriber = Subscriber(topic=topic, id=id)
+      self.future = Future()
+    def done(self):
+      return self.future.done()
+    def result(self):
+      return self.future.result()
+    def run(self):
+      try:
+        self.subscriber.open()
+        result = self.subscriber.receive() # blocks until message arrival
+        self.subscriber.close()
+        self.future.set_result(result)
+      except Exception as error:
+        self.future.set_exception(error)
+
+  def test_construction(self, tmpdir):
+    topic = Topic(path=tmpdir.join('foobar'))
+    with Publisher(topic) as publisher:
+      assert publisher.topic == topic
+
+  def test_publish_with_no_subscribers(self, tmpdir):
+    topic = Topic(path=tmpdir.join('foobar'))
+    message = "Hello, world!"
+    with Publisher(topic) as publisher:
+      assert publisher.publish(message) == set()
+
+  def test_publish_with_one_subscriber(self, tmpdir):
+    topic = Topic(path=tmpdir.join('foobar'))
+    message = "Hello, world!"
+    with Publisher(topic) as publisher:
+      thread = self.SubscriberThread(topic, 1)
+      thread.start()
+      sleep(0.25)    # wait for subscribers
+      assert publisher.publish(message) == set((1,))
+      thread.join()  # wait for subscribers
+      assert thread.done()
+      assert thread.result() == message
+
+  def test_publish_with_two_subscribers(self, tmpdir):
+    topic = Topic(path=tmpdir.join('foobar'))
+    message = "Hello, world!"
+    with Publisher(topic) as publisher:
+      thread1 = self.SubscriberThread(topic, 1)
+      thread1.start()
+      thread2 = self.SubscriberThread(topic, 2)
+      thread2.start()
+      sleep(0.25)    # wait for subscribers
+      assert publisher.publish(message) == set((1, 2))
+      thread1.join() # wait for subscribers
+      assert thread1.done()
+      assert thread1.result() == message
+      thread2.join() # wait for subscribers
+      assert thread2.done()
+      assert thread2.result() == message
 
 if __name__ == '__main__':
   import pytest, sys
