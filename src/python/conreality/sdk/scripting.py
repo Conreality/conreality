4f59cad3d95dd362612957eedda90884b93826d9
Arto Bendiken <arto@bendiken.net>
2016-03-31 00:13:42 +0200

    Implemented conreality.sdk.scripting.Context#prepend_package_path().

--- src/python/conreality/sdk/scripting.py
+++ src/python/conreality/sdk/scripting.py
@@ -10,19 +10,29 @@ class Context:
 
   @property
   def package_path(self):
-    return self.runtime.globals().package.path
+    return self.runtime.globals().package.path.split(';')
 
   @package_path.setter
   def package_path(self, value):
+    if type(value) is list:
+      value = ';'.join(value)
     self.runtime.globals().package.path = value
 
   @property
   def package_cpath(self):
-    return self.runtime.globals().package.cpath
+    return self.runtime.globals().package.cpath.split(';')
 
   @package_cpath.setter
   def package_cpath(self, value):
+    if type(value) is list:
+      value = ';'.join(value)
     self.runtime.globals().package.cpath = value
+
+  def prepend_package_path(self, path):
+    package_path = self.package_path
+    package_path.insert(0, str(path))
+    self.package_path = package_path
+    return self
 
   def load_code(self, code):
     self.eval_code(code)
