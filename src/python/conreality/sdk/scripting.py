fec6bd2b8cd7a115891b244b24ab4a1c9006791d
Arto Bendiken <arto@bendiken.net>
2016-03-30 21:20:49 +0200

    Implemented conreality.sdk.scripting.Context#package_path.

--- src/python/conreality/sdk/scripting.py
+++ src/python/conreality/sdk/scripting.py
@@ -8,6 +8,22 @@ class Context:
   def __init__(self):
     self.runtime = lupa.LuaRuntime(unpack_returned_tuples=True)
 
+  @property
+  def package_path(self):
+    return self.runtime.globals().package.path
+
+  @package_path.setter
+  def package_path(self, value):
+    self.runtime.globals().package.path = value
+
+  @property
+  def package_cpath(self):
+    return self.runtime.globals().package.cpath
+
+  @package_cpath.setter
+  def package_cpath(self, value):
+    self.runtime.globals().package.cpath = value
+
   def load_code(self, code):
     self.eval_code(code)
 
@@ -19,7 +35,8 @@ class Context:
 
   def eval_file(self, filepath):
     with open(filepath, 'r') as file:
-      return self.runtime.eval(file.read())
+      code = file.read()
+      return self.eval_code(code)
 
   def define(self, name, function):
     self.runtime.globals()[name] = function
