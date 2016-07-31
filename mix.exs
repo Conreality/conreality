b223ae2d89ea6256858afd27a4f96d733120dafe
Arto Bendiken <arto@bendiken.net>
2016-07-31 17:36:17 +0200

    Imported the output of `mix nerves.new conreality --target qemu_arm`.

--- mix.exs
+++ mix.exs
@@ -1,32 +1,40 @@
 defmodule Conreality.Mixfile do
   use Mix.Project
 
+  @target System.get_env("NERVES_TARGET") || "qemu_arm"
+
   def project do
     [app: :conreality,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: "0.0.1",
+     target: @target,
+     archives: [nerves_bootstrap: "~> 0.1.4"],
+     deps_path: "deps/#{@target}",
+     build_path: "_build/#{@target}",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     aliases: aliases,
+     deps: deps ++ system(@target)]
   end
 
-  # Configuration for the OTP application
+  # Configuration for the OTP application.
   #
-  # Type "mix help compile.app" for more information
+  # Type `mix help compile.app` for more information.
   def application do
-    [applications: [:logger]]
+    [mod: {Conreality, []},
+     applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  def deps do
+    [{:nerves, "~> 0.3.0"}]
   end
+
+  def system(target) do
+    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  end
+
+  def aliases do
+    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  end
+
 end
