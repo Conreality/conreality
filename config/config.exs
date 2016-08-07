b5c76b8976d17f6a181c37aedd4647b8a2f2c132
Arto Bendiken <arto@bendiken.net>
2016-08-07 02:41:48 +0200

    Added /etc/conreality/system.conf as a rootfs overlay.

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,9 @@
 # is restricted to this project.
 use Mix.Config
 
+config :nerves, :firmware,
+  rootfs_additions: "config/#{Mix.Project.config[:target]}"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
