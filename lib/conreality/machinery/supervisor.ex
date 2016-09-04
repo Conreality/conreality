ab36ac26f40e8cc5c54a1a11309e6940e5df1aee
Arto Bendiken <arto@bendiken.net>
2016-09-04 02:52:11 +0200

    Implemented hardware hotplug monitoring.

--- lib/conreality/machinery/supervisor.ex
+++ lib/conreality/machinery/supervisor.ex
@@ -14,10 +14,12 @@ defmodule Conreality.Machinery.Supervisor do
   use Supervisor
   require Logger
 
+  @spec start_link() :: {:ok, pid}
   def start_link do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @spec init([]) :: {:ok, {:supervisor.sup_flags, [Supervisor.Spec.spec]}}
   def init([]) do
     children = [
       worker(Conreality.Machinery.Supervisor.Discovery, [], restart: :transient)
@@ -26,12 +28,14 @@ defmodule Conreality.Machinery.Supervisor do
   end
 
   defmodule Discovery do
+    @spec start_link() :: {:ok, pid}
     def start_link do
       Logger.info "Starting hardware discovery..."
 
       {:ok, spawn_link(__MODULE__, :init, [])}
     end
 
+    @spec init() :: none
     def init do
       Logger.info "Performing hardware discovery..."
 
@@ -43,14 +47,51 @@ defmodule Conreality.Machinery.Supervisor do
   end
 
   defmodule Monitoring do
+    @spec start_link() :: {:ok, pid}
     def start_link do
       Logger.info "Starting hardware monitoring..."
 
       {:ok, spawn_link(__MODULE__, :init, [])}
     end
 
+    @spec init() :: no_return
     def init do
-      :timer.sleep(1_000_000) # TODO: monitor udev events
+      priv_dir = :code.priv_dir(:conreality)
+
+      port = Port.open({:spawn_executable, "/usr/bin/env"},
+        [:binary, :in,
+         {:packet, 4},
+         {:args, ["python3", "-u", "#{priv_dir}/udev-monitor.py"]},
+         {:env, [{'PYTHONPATH', 'src/python'}]},
+         :exit_status,
+         :nouse_stdio])
+
+      loop(port)
+    end
+
+    @spec loop(port) :: no_return
+    def loop(port) do
+      event = read(port)
+
+      IO.inspect event # TODO
+
+      loop(port)
+    end
+
+    @spec read(port) :: no_return | term
+    def read(port) do
+      receive do
+        {^port, {:exit_status, code}} ->
+          Logger.warn "Hardware monitoring process exited with code #{code}."
+          exit({:shutdown, code})
+
+        {^port, {:data, message}} ->
+          :erlang.binary_to_term(message)
+
+        garbage ->
+          Logger.error "Hardware monitoring process received an unexpected message: #{inspect garbage}."
+          :erlang.error({:badmatch, garbage})
+      end
     end
   end
 end
