64749392820e728fe4729b67698c23b92cd5664c
Arto Bendiken <arto@bendiken.net>
2016-09-08 01:18:43 +0200

    Implemented Conreality.Scripting.path().

--- lib/conreality/scripting.ex
+++ lib/conreality/scripting.ex
@@ -4,6 +4,11 @@ defmodule Conreality.Scripting do
   @moduledoc """
   """
 
+  @spec path() :: binary
+  def path do
+    Path.join(:code.priv_dir(:conreality), "lua")
+  end
+
   @spec start_link(binary, Lua.State.t) :: {:ok, pid} | {:error, any, any}
   def start_link(filepath, state \\ nil) when is_binary(filepath) do
     Lua.Thread.start_link(filepath, state)
