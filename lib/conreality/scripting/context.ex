2f854cf10754d48ff7cddeb52aa57f973f99f429
Arto Bendiken <arto@bendiken.net>
2016-09-09 22:18:23 +0200

    Implemented a scripting prelude to import all SDK modules.

--- lib/conreality/scripting/context.ex
+++ lib/conreality/scripting/context.ex
@@ -12,12 +12,20 @@ defmodule Conreality.Scripting.Context do
     Lua.State.new
     |> Lua.set_package_path(Scripting.package_path())
     |> load_sdk()
+    |> exec_prelude()
     |> Lua.gc
   end
 
   @spec load_sdk(Lua.State.t) :: Lua.State.t
   def load_sdk(state) do
-    {state, [table]} = state |> Lua.require!("conreality.sdk")
+    {state, [_]} = state |> Lua.require!("conreality.sdk")
     state
+  end
+
+  @spec exec_prelude(Lua.State.t) :: Lua.State.t
+  def exec_prelude(state) do
+    state = Lua.State.unwrap(state)
+    {_, state} = :luerl.dofile(Scripting.path_to("prelude.lua") |> String.to_charlist, state)
+    Lua.State.wrap(state)
   end
 end
---
+++ priv/lua/prelude.lua
@@ -0,0 +1,11 @@
+-- This is free and unencumbered software released into the public domain.
+
+--- Conreality Software Development Kit (SDK) prelude
+
+for module, module_table in pairs(require('conreality.sdk')) do
+  if type(module_table) == 'table' then
+    for variable, value in pairs(module_table) do
+      _G[variable] = value
+    end
+  end
+end
